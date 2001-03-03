cb53c328526fbc5de85b27729f0753ab96c43ffe
cell <cell>
2001-03-03 19:00:17 +0000

    removed app-signature field, as it's not really useful, the version field now resorts to major/middle/minor/etc if long_info is not set in the apps version_info

--- InfoView/AppNodeInfoView.cpp
+++ InfoView/AppNodeInfoView.cpp
@@ -29,12 +29,11 @@ __USE_CORTEX_NAMESPACE
 
 AppNodeInfoView::AppNodeInfoView(
 	const NodeRef *ref)
-	: LiveNodeInfoView(ref)
-{
+	: LiveNodeInfoView(ref) {
 	D_METHOD(("AppNodeInfoView::AppNodeInfoView()\n"));
 
 	// adjust view properties
-	setSideBarWidth(be_plain_font->StringWidth(" File Format ") + 2 * InfoView::M_H_MARGIN);
+	setSideBarWidth(be_plain_font->StringWidth(" Application ") + 2 * InfoView::M_H_MARGIN);
 	setSubTitle("Application-Owned Node");
 
 	// add separator
@@ -44,34 +43,61 @@ AppNodeInfoView::AppNodeInfoView(
 	app_info appInfo;
 
 	if ((get_port_info(ref->node().port, &portInfo) == B_OK)
-	 && (be_roster->GetRunningAppInfo(portInfo.team, &appInfo) == B_OK))
-	{
+	 && (be_roster->GetRunningAppInfo(portInfo.team, &appInfo) == B_OK)) {
 		BEntry appEntry(&appInfo.ref);
 		char appName[B_FILE_NAME_LENGTH];
 		if ((appEntry.InitCheck() == B_OK)
-		 && (appEntry.GetName(appName) == B_OK))
-		{
+		 && (appEntry.GetName(appName) == B_OK)) {
 			addField("Application", appName);
 		}
 		BFile appFile(&appInfo.ref, B_READ_ONLY);
-		if (appFile.InitCheck() == B_OK)
-		{
+		if (appFile.InitCheck() == B_OK) {
 			BAppFileInfo appFileInfo(&appFile);
-			if (appFileInfo.InitCheck() == B_OK)
-			{
+			if (appFileInfo.InitCheck() == B_OK) {
 				version_info appVersion;
-				if (appFileInfo.GetVersionInfo(&appVersion, B_APP_VERSION_KIND) == B_OK)
-				{
-					addField("Version", appVersion.long_info);
+				if (appFileInfo.GetVersionInfo(&appVersion, B_APP_VERSION_KIND) == B_OK) {
+					BString versionStr("");
+					if (strlen(appVersion.long_info) != 0) {
+						versionStr << appVersion.long_info;
+					}
+					else if (strlen(appVersion.short_info) != 0) {
+						versionStr << appVersion.short_info;
+					}
+					else {
+						versionStr << appVersion.major << ".";
+						versionStr << appVersion.middle << ".";
+						versionStr << appVersion.minor;
+						switch (appVersion.variety) {
+							case 0: {
+								versionStr << " Development " << appVersion.internal;
+								break;
+							}
+							case 1: {
+								versionStr << " Alpha " << appVersion.internal;
+								break;
+							}
+							case 2: {
+								versionStr << " Beta " << appVersion.internal;
+								break;
+							}
+							case 3: {
+								versionStr << " Gamma " << appVersion.internal;
+								break;
+							}
+							case 4: {
+								versionStr << " Golden Master " << appVersion.internal << "";
+								break;
+							}
+						}
+					}
+					addField("Version", versionStr);
 				}
 			}
 		}
-		addField("Signature", appInfo.signature);
 	}
 }
 
-AppNodeInfoView::~AppNodeInfoView()
-{
+AppNodeInfoView::~AppNodeInfoView() {
 	D_METHOD(("AppNodeInfoView::~AppNodeInfoView()\n"));
 }
 
